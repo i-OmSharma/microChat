9ae990ed04fe232ebe5adbe42318f2f9252b2a5e
i-OmSharma <sharmaom1201@gmail.com>
2025-08-27 23:28:21 +0530

    intial

---
+++ backend/user/dist/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { createClient } from 'redis';
+import userRoutes from "./routes/user.js";
+dotenv.config();
+connectDb();
+export const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
+redisClient
+    .connect()
+    .then(() => console.log('Redis Connected'))
+    .catch(console.error);
+const app = express();
+app.use("api/v1/", userRoutes);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
